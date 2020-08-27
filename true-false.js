85e4ace5d446379558a691afed03431e4f52370a
mridha md.mekail <mazharulaminmasum@gmail.com>
2020-08-27 10:37:07 +0600

    true vs false

---
+++ true-false.js
@@ -0,0 +1,21 @@
+/*false value is:
+0
+""
+undefined
+null
+NaN*/
+
+/*Truth value = 
+"0", " ", {}, [], false
+*/
+
+//const name  = "0 ";
+let name = 12;
+console.log(name);
+if( name || name ==0){
+    console.log('condition is true');
+
+}
+else{
+    console.log('condition is false');
+}
